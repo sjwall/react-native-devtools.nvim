f67d6c3adaf0b2bf7d59c3e2c04e879636382789
Samuel Wall <me@samuelwall.uk>
2025-08-05 05:23:59 +0000

    refactor(logger): change log file path

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,7 +1,15 @@
 import {appendFile} from 'node:fs/promises'
+import {join} from 'node:path'
+import {NvimPlugin} from 'neovim'
 
 export class Logger {
   #filePath = './log.txt'
+
+  constructor(plugin: NvimPlugin) {
+    plugin.nvim.call('stdpath', 'log').then((value) => {
+      this.#filePath = join(value, 'react-native-devtools.txt')
+    })
+  }
 
   async log(...message: string[]) {
     await appendFile(this.#filePath, `INFO: ${message.join(' ')}\n`)
