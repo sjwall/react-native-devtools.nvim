8f3677a223b57b1703e33cdc3f93a8e33a99a7b6
Samuel Wall <me@samuelwall.uk>
2025-08-07 20:10:13 +0000

    feat(console): set custom filetype

--- src/utils/createConsoleBuffer.ts
+++ src/utils/createConsoleBuffer.ts
@@ -4,6 +4,7 @@ export async function createConsoleBuffer(plugin: NvimPlugin) {
   await plugin.nvim.command('enew')
   const buffer = await plugin.nvim.buffer
   await buffer.setOption('buftype', 'nofile')
+  await buffer.setOption('filetype', 'react-native-devtools-console')
   // await buf.setOption('bufhidden', 'hide');
   await buffer.setOption('swapfile', false)
   await buffer.setLines([], {
