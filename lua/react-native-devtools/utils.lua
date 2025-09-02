ebfbabd6056930538e121586efb285574691f207
Samuel Wall <me@samuelwall.uk>
2025-09-02 20:23:43 +0000

    chore(utils): add lua function to get plugin dir path

--- lua/react-native-devtools/utils.lua
+++ lua/react-native-devtools/utils.lua
@@ -1,3 +1,5 @@
+local Path = require("plenary.path")
+
 local M = {}
 
 ---@param str string
@@ -5,6 +7,13 @@ local M = {}
 ---@return integer count
 function M.trim(str)
   return str:gsub("^%s+", ""):gsub("%s+$", "")
+end
+
+---@return Path path
+function M.get_plugin_dir()
+  local info = debug.getinfo(1, "S")
+  local script_path = info.source:sub(2)
+  return Path:new(script_path):parent():parent():parent()
 end
 
 return M
